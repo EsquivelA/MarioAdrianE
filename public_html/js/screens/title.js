bea1d1a7b49aa08650e14e5a1777d88c7dadc343
Adrian Esquivel <Adrianesquivel1112@yahoo.com>
2014-12-03 11:25:19 -0800

    title screen
    finished the title screen had to fix errors

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,7 +3,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		me.game.world.addChildd(new me.Sprite (0, 0, me.loader.getImage('title-screen')), -10);
+		me.game.world.addChild(new me.Sprite (0, 0, me.loader.getImage('title-screen')), -10);
                 me.input.bindKey(me.input.KEY.ENTER, "start");
                 
                 me.game.world.addChild(new (me.Renderable.extend ({
@@ -19,7 +19,7 @@ game.TitleScreen = me.ScreenObject.extend({
                     
                 })));
                 
-                this.handler = me.event.suscribe(me.event.KEYDOWN, function(action, keyCode, edge) {
+                this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge) {
                     if(action ===  "start"){
                         me.state.change(me.state.PLAY);
                     }
