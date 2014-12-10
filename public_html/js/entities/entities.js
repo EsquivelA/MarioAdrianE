c94fbaa6feb7e764db431028ceb4d61fb2185250
Adrian Esquivel <Adrianesquivel1112@yahoo.com>
2014-12-09 20:34:01 -0800

    walk left
    made player walk left and made the mushroom work

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -15,13 +15,20 @@ game.PlayerEntity = me.Entity.extend({
             }]);
 
         this.renderable.addAnimation("idle", [3]);
+        this.renderable.addAnimation("bigIdle", [19]);
         //creates an animation for the character walking without powerups called smallwalk
         //adds an arrey of values 8-13 which are the pictures for the animation 
         //the speed for the character
         this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
+        this.renderable.addAnimation("bigWalk", [14, 15, 16, 17, 18, 19], 80);
+        this.renderable.addAnimation("shrink", [0, 1, 2, 3], 80);
+        this.renderable.addAnimation("grow", [4, 5, 6, 7], 80);
+        
+        
 
         this.renderable.setCurrentAnimation("idle");
 
+        this.big = false;
         //the first number sets the speed mario moves on x aaxis, the second sets the speed ont the y axis
         this.body.setVelocity(5, 20);
 
@@ -31,26 +38,43 @@ game.PlayerEntity = me.Entity.extend({
     },
     update: function(delta) {
         //checks if the right key is pushed down
-        if (me.input.isKeyPressed("right")) {
+        if (me.input.isKeyPressed("left")) {
+            this.flipX(true);
             //adds value to mario's x position based on the x value from setVelocity above
             //me.timer.tick smooths the animation for irregular updates
+            this.body.vel.x -= this.body.accel.x * me.timer.tick;
+         
+        }else if (me.input.isKeyPressed("right")) {
+            this.flipX(false);
             this.body.vel.x += this.body.accel.x * me.timer.tick;
-
+           
         } else {
             //velocity of x equals 0
             this.body.vel.x = 0;
+         
         }
 
         this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
 
+    if(!this.big){
         if (this.body.vel.x !== 0) {
-            if (!this.renderable.isCurrentAnimation("smallWalk")) {
+            if (!this.renderable.isCurrentAnimation("smallWalk") && !this.renderable.isCurrentAnimation("grow") && !this.renderable.isCurrentAnimation("shrink")) {
                 this.renderable.setCurrentAnimation("smallWalk");
                 this.renderable.setAnimationFrame();
             }
         } else {
             this.renderable.setCurrentAnimation("idle");
+            }
+        }else{
+             if (this.body.vel.x !== 0) {
+            if (!this.renderable.isCurrentAnimation("bigWalk") && !this.renderable.isCurrentAnimation("grow") && !this.renderable.isCurrentAnimation("shrink")) {
+                this.renderable.setCurrentAnimation("bigWalk");
+                this.renderable.setAnimationFrame();
+            }
+        } else {
+            this.renderable.setCurrentAnimation("bigIdle");
+            }
         }
 
         this._super(me.Entity, "update", [delta]);
@@ -65,8 +89,20 @@ game.PlayerEntity = me.Entity.extend({
             if (ydif <= -115) {
                 response.b.allive = false;
             } else {
-                me.state.change(me.state.MENU);
+                if(this.big){
+                    this.big = false;
+                    this.body.vel.y -= this.body.accel.y * me.timer.tick;
+                    this.jumping = true;
+                    this.renderable.setCurrentAnimation("shrink", "idle");
+                    this.renderable.setAnimationFrame();
+                }else{
+                     me.state.change(me.state.MENU);
+                }
             }
+        }else if(response.b.type === 'mushroom') {
+            this.renderable.setCurrentAnimation("grow", "bigIdle");
+            this.big = true;
+            me.game.world.removeChild(response.b);
         }
 
     }
